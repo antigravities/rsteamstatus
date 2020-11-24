42b9f5469f2a855d0ce5be31a982b4d1fc92fe43
Alexandra Frock <im@alexandra.moe>
2020-11-24 13:15:18 -0500

    switch around assert args, oops

--- main_test.go
+++ main_test.go
@@ -9,20 +9,20 @@ import (
 func TestIsGoodReturnsProperly(t *testing.T) {
 	assert := assert.New(t)
 
-	assert.Equal(isGood("92.5% Online"), "good")
-	assert.Equal(isGood("Normal"), "good")
-	assert.Equal(isGood("nOrMaL"), "good")
-	assert.Equal(isGood("1"), "bad")
-	assert.Equal(isGood("2"), "bad")
-	assert.Equal(isGood("Service Unavailable"), "bad")
+	assert.Equal("good", isGood("92.5% Online"))
+	assert.Equal("good", isGood("Normal"))
+	assert.Equal("good", isGood("nOrMaL"))
+	assert.Equal("bad", isGood("1"))
+	assert.Equal("bad", isGood("2"))
+	assert.Equal("bad", isGood("Service Unavailable"))
 }
 
 func TestCrowbarReturnsValidResult(t *testing.T) {
 	assert := assert.New(t)
 	status, err := fetchStatus()
 
-	assert.NotEqual(status, nil)
-	assert.Equal(err, nil)
+	assert.NotEqual(nil, status)
+	assert.Equal(nil, err)
 	assert.NotEmpty(status.Services)
 	assert.Less(int64(1606241083), status.Time)
 	assert.Less(float32(0), status.Online)
@@ -31,10 +31,10 @@ func TestCrowbarReturnsValidResult(t *testing.T) {
 func TestReturnsValidRedditInstance(t *testing.T) {
 	reddit, err := makeReddit()
 
-	assert.NotEqual(t, reddit, nil)
-	assert.Equal(t, err, nil)
+	assert.NotEqual(t, nil, reddit)
+	assert.Equal(t, nil, err)
 }
 
 func TestNormalRun(t *testing.T) {
-	assert.Equal(t, run(), nil)
+	assert.Equal(t, nil, run())
 }
